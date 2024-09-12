3be12aa4008d9ff4f9708c8d4efc21fbe8654682
hapqi210 <157613652+hapqi210@users.noreply.github.com>
2024-09-12 15:18:29 +0700

    Add files via upload

--- busohaki.lua
+++ busohaki.lua
@@ -1,15 +1,23 @@
+
 local player = game.Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 local humanoid = character:WaitForChild("Humanoid")
-
 local function activateBuso()
-    if not character:FindFirstChild("Buso") then
-        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("Buso")
-    end
+if not character:FindFirstChild("Buso") then
+game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("Buso")
 end
-
-while true do
-    activateBuso()
-    wait(1) 
 end
-  
+local function onHumanoidDied()
+humanoid.Died:Connect(function()
+character = player.Character or player.CharacterAdded:Wait()
+humanoid = character:WaitForChild("Humanoid")
+wait(5)
+activateBuso()
+end)
+end
+onHumanoidDied()
+player.CharacterAdded:Connect(function(newCharacter)
+character = newCharacter
+humanoid = newCharacter:WaitForChild("Humanoid")
+onHumanoidDied()
+end)
